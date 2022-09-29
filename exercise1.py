d9227428ab0beef901e65aaf5c6cd40b8e704c79
Yevgeny Omar Siregar <siregar.yevgeny@gmail.com>
2022-09-29 19:51:58 +0700

    exercise 1

---
+++ exercise1.py
@@ -0,0 +1,16 @@
+#input amount of bill
+billAmount = float(input("Enter the amount of bill(USD): "))
+
+#input amount of people
+totalPerson = int(input("Enter the amount of people: "))
+
+#input amount of tips
+totalTips = float(input("Enter the amount of tips(%): "))
+
+#formula for bill per person and tips per person
+totalBillPerPerson = ((((billAmount * totalTips) / 100) + billAmount) / totalPerson)
+tipsPerPerson = totalTips /5 totalPerson
+
+#print output
+print(f"Total bill per person(USD): {totalBillPerPerson}")
+print(f"Tips per person(%): {tipsPerPerson}")
